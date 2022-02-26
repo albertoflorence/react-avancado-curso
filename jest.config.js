91b4ab311250779999c611a07d6650733c3014a1
albertoflorence <alberto.sabatier@gmail.com>
2022-02-26 20:05:22 -0300

    fix jest collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: 'jsdom',
-  collectCoverageFrom: ['src/**/*/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*/*.ts(x)?', '!src/**/*/*.stories.tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   transform: {
