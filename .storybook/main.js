ef911c10afecfd93f3a9e53b8846cc58834725db
albertoflorence <alberto.sabatier@gmail.com>
2022-03-02 16:52:12 -0300

    update storybook to use next-router addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,12 @@
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
-  ],
-  framework: '@storybook/react',
+  addons: ['@storybook/addon-interactions', 'storybook-addon-next-router'],
   core: {
     builder: 'webpack5'
+  },
+  webpackFinal: config => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
   }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,9 @@
 import GlobalStyles from '../src/styles/global'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
-    }
+  nextRouter: {
+    Provider: RouterContext.Provider
   }
 }
 
