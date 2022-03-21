f1539734b3be1c6e2bcfa2d5d734373b0487b20b
albertoflorence <alberto.sabatier@gmail.com>
2022-03-21 14:32:21 -0300

    fix Game prop mock

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -24,9 +24,7 @@ export async function getStaticProps(): Promise<{ props: GameTemplateProps }> {
         description:
           'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification.You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality.'
       },
-      gallery: {
-        items: galleryMock
-      },
+      gallery: galleryMock,
       description: descriptionHTML,
       gameDetails: {
         developer: 'CD PROJEKT RED',
