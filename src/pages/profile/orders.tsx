b891f93b396f5da507329f0b422de5f417e18751
albertoflorence <alberto.sabatier@gmail.com>
2022-03-24 14:20:12 -0300

    add orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,5 +1,19 @@
+import OrdersList, { OrdersListProps } from 'components/OrdersList'
+import mock from 'components/OrdersList/mock'
 import Profile from 'templates/Profile'
 
-export default function Index() {
-  return <Profile activeLink={'/profile/orders'}>Orders</Profile>
+export default function Index(props: OrdersListProps) {
+  return (
+    <Profile activeLink={'/profile/orders'}>
+      <OrdersList {...props} />
+    </Profile>
+  )
+}
+
+export async function getStaticProps(): Promise<{ props: OrdersListProps }> {
+  return {
+    props: {
+      items: mock
+    }
+  }
 }
