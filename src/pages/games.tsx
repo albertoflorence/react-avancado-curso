066005a55318c351fd2a67ec5a19fc3083374f84
albertoflorence <alberto@alberto.localdomain>
2022-03-29 10:02:44 -0300

    update games page to fetch data from the api

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,17 +1,38 @@
+import { GetStaticPropsResult } from 'next'
 import { FilterProps } from 'components/ExploreSidebar'
 import filtersMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/queries'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export default function Index(props: GamesTemplateProps) {
   return <GamesTemplate {...props}></GamesTemplate>
 }
 
-export function getStaticProps(): { props: GamesTemplateProps } {
+export async function getStaticProps(): Promise<GetStaticPropsResult<GamesTemplateProps>> {
+  const client = initializeApollo()
+
+  const { data } = await client.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  })
+
   return {
     props: {
       filters: filtersMock as FilterProps[],
-      games: gamesMock
+      games: data.games.map(game => ({
+        slug: game.slug,
+        title: game.name,
+        image: `http://localhost:1337${game.cover?.url}`,
+        subtitle: game.developers[0].name,
+        price: new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
+          game.price
+        ),
+        favorite: false
+      }))
     }
   }
 }
