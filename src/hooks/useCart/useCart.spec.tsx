e1415698671a6d993e4275eb1c867c8ae4d235b3
albertoflorence <alberto@alberto.localdomain>
2022-04-07 19:19:43 -0300

    add actions to useCart

--- src/hooks/useCart/useCart.spec.tsx
+++ src/hooks/useCart/useCart.spec.tsx
@@ -1,6 +1,6 @@
 import { useCart, CartProvider, CartProviderProps } from './useCart'
-import { renderHook } from '@testing-library/react-hooks'
-import { setStorageItem } from 'utils/localStorage'
+import { act, renderHook } from '@testing-library/react-hooks'
+import { getStorageItem, setStorageItem } from 'utils/localStorage'
 import { MockedProvider, MockedResponse } from '@apollo/client/testing'
 import { gamesMock } from './mock'
 
@@ -15,27 +15,43 @@ const init = (props: MockedResponse[]) => {
 }
 
 describe('useCart()', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
   it('should return cart items default values', async () => {
-    const { result, waitForNextUpdate } = init([])
-    await waitForNextUpdate()
+    const { result } = init([])
+
+    act(() => {
+      result.current.addItem('')
+      result.current.removeItem('')
+      result.current.clear()
+    })
 
     expect(result.current.items).toEqual([])
     expect(result.current.total).toBe('$0.00')
     expect(result.current.quantity).toBe(0)
+    expect(result.current.loading).toBe(true)
+    expect(result.current.hasItem('')).toBe(false)
   })
 
   it('should return cart items', async () => {
     setStorageItem('cartItems', ['any-slug', 'another-slug'])
     const { result, waitForNextUpdate } = init([gamesMock])
+
+    expect(result.current.loading).toBe(true)
     await waitForNextUpdate()
 
+    expect(result.current.loading).toBe(false)
     expect(result.current.items).toEqual([
       {
+        slug: 'any-slug',
         title: 'any game',
         image: 'http://localhost:1337/any_url',
         price: '$100.00'
       },
       {
+        slug: 'another-slug',
         title: 'another game',
         image: 'http://localhost:1337/any_url',
         price: '$200.00'
@@ -43,5 +59,48 @@ describe('useCart()', () => {
     ])
     expect(result.current.total).toBe('$300.00')
     expect(result.current.quantity).toBe(2)
+  })
+
+  it('should return true/false if cart item exist', () => {
+    setStorageItem('cartItems', ['any-slug'])
+    const { result } = init([])
+
+    expect(result.current.hasItem('any-slug')).toBe(true)
+    expect(result.current.hasItem('another-slug')).toBe(false)
+  })
+
+  it('should add item in the cart', async () => {
+    const { result } = init([])
+
+    act(() => {
+      result.current.addItem('any-slug')
+    })
+
+    expect(result.current.quantity).toBe(1)
+    expect(getStorageItem('cartItems')).toEqual(['any-slug'])
+  })
+
+  it('should remove item from the cart', () => {
+    setStorageItem('cartItems', ['any-slug'])
+    const { result } = init([])
+
+    act(() => {
+      result.current.removeItem('any-slug')
+    })
+
+    expect(result.current.quantity).toBe(0)
+    expect(getStorageItem('cartItems')).toEqual([])
+  })
+
+  it('should clear the cart', () => {
+    setStorageItem('cartItems', ['any-slug', 'another-slug'])
+    const { result } = init([])
+
+    act(() => {
+      result.current.clear()
+    })
+
+    expect(result.current.quantity).toBe(0)
+    expect(getStorageItem('cartItems')).toEqual([])
   })
 })
--- src/hooks/useCart/useCart.tsx
+++ src/hooks/useCart/useCart.tsx
@@ -1,24 +1,35 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { useGetGames } from 'services'
 import { formatPrice, formattedPriceToNumber } from 'utils/helpers'
-import { getStorageItem } from 'utils/localStorage'
+import { getStorageItem, setStorageItem } from 'utils/localStorage'
 
 interface CartItem {
   title: string
   image: string
   price: string
+  slug: string
 }
 
 export type CartContextData = {
   items: CartItem[]
   total: string
   quantity: number
+  loading: boolean
+  hasItem: (slug: string) => boolean
+  addItem: (slug: string) => void
+  removeItem: (slug: string) => void
+  clear: () => void
 }
 
 export const defaultValues: CartContextData = {
   items: [],
   total: '$0.00',
-  quantity: 0
+  quantity: 0,
+  hasItem: () => false,
+  addItem: () => undefined,
+  removeItem: () => undefined,
+  clear: () => undefined,
+  loading: true
 }
 
 export const CartContext = createContext<CartContextData>(defaultValues)
@@ -29,6 +40,7 @@ export interface CartProviderProps {
 
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [cartItems, setCartItems] = useState<string[]>([])
+
   useEffect(() => {
     const data = getStorageItem<string[]>('cartItems')
     if (data) {
@@ -36,7 +48,11 @@ export const CartProvider = ({ children }: CartProviderProps) => {
     }
   }, [])
 
-  const { data } = useGetGames({
+  useEffect(() => {
+    setStorageItem('cartItems', cartItems)
+  }, [cartItems])
+
+  const { data, loading } = useGetGames({
     variables: {
       where: {
         slug: cartItems
@@ -52,12 +68,23 @@ export const CartProvider = ({ children }: CartProviderProps) => {
     data?.map(game => ({
       image: game.image,
       price: game.price,
-      title: game.title
+      title: game.title,
+      slug: game.slug
     })) || []
 
-  const quantity = items.length
+  const quantity = cartItems.length
+  const hasItem = (slug: string) => cartItems.includes(slug)
+  const addItem = (slug: string) => setCartItems(state => state.concat(slug))
+  const removeItem = (slug: string) => setCartItems(state => state.filter(item => item !== slug))
+  const clear = () => setCartItems([])
 
-  return <CartContext.Provider value={{ total, items, quantity }}>{children}</CartContext.Provider>
+  return (
+    <CartContext.Provider
+      value={{ total, items, quantity, loading, hasItem, addItem, removeItem, clear }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
 
 export const useCart = () => useContext(CartContext)
