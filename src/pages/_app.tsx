2e9e37b420a26a64014c65415263edaf41d02659
albertoflorence <alberto@alberto.localdomain>
2022-04-08 18:57:41 -0300

    add NextAuth and its configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,37 @@
-import { ApolloProvider } from '@apollo/client'
-import { CartProvider } from 'hooks'
-import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
-import theme from 'styles/theme'
-import { useApollo } from 'utils/apollo'
 import NextNProgress from 'nextjs-progressbar'
+
+import { SessionProvider } from 'next-auth/react'
+import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from 'styled-components'
+import { CartProvider } from 'hooks'
+
+import { useApollo } from 'utils/apollo'
+import { AppProps } from 'next/app'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>React Avançado</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="description" content="A simple project" />
-          </Head>
-          <GlobalStyles />
-          <NextNProgress color={theme.colors.primary} height={3} />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <SessionProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>React Avançado</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta name="description" content="A simple project" />
+            </Head>
+            <GlobalStyles />
+            <NextNProgress color={theme.colors.primary} height={3} />
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </SessionProvider>
   )
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+
+const options: NextAuthOptions = {
+  pages: {
+    signIn: '/login'
+  },
+  providers: [
+    Credentials({
+      name: 'Login',
+      credentials: { email: {}, password: {} },
+      async authorize(credentials) {
+        if (!credentials) return null
+
+        const { email, password } = credentials
+        const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/auth/local', {
+          method: 'POST',
+          body: new URLSearchParams({ identifier: email, password })
+        })
+
+        const data = await response.json()
+
+        if (data.user) {
+          return { ...data.user, jwt: data.jw }
+        }
+
+        return null
+      }
+    })
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.jwt = user.jwt
+      session.id = user.id
+
+      return Promise.resolve(session)
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.email = user.username as string
+        token.jwt = user.jwt
+      }
+      return Promise.resolve(token)
+    }
+  }
+}
+
+export const Auth = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options)
