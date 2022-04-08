7854680e4824b6840ee5e36ae34e5f5b15a71155
albertoflorence <alberto@alberto.localdomain>
2022-04-08 17:06:37 -0300

    add createUser to FormSigUp

--- src/components/FormSignUp/FormSignUp.spec.tsx
+++ src/components/FormSignUp/FormSignUp.spec.tsx
@@ -1,22 +1,28 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { render, screen } from 'utils/tests'
 import FormSignUp from './FormSignUp'
 
 const init = () => {
-  render(<FormSignUp />)
+  render(
+    <MockedProvider>
+      <FormSignUp />
+    </MockedProvider>
+  )
 }
 
 const textField = (name: string) => screen.getByPlaceholderText(name)
 const link = (name: string) => screen.getByRole('link', { name: new RegExp(name, 'i') })
+const button = (name: string) => screen.getByRole('button', { name: new RegExp(name, 'i') })
 
 describe('<FormSignUp />', () => {
   it('should render correctly', () => {
     init()
 
-    expect(textField('Name')).toBeInTheDocument()
+    expect(textField('Username')).toBeInTheDocument()
     expect(textField('Email')).toBeInTheDocument()
     expect(textField('Password')).toBeInTheDocument()
     expect(textField('Confirm password')).toBeInTheDocument()
-    expect(link('sign up now')).toBeInTheDocument()
+    expect(button('sign up now')).toBeInTheDocument()
     expect(link('sign in')).toHaveAttribute('href', '/login')
   })
 })
--- src/components/FormSignUp/FormSignUp.tsx
+++ src/components/FormSignUp/FormSignUp.tsx
@@ -3,33 +3,86 @@ import Icon from 'components/Icon'
 import Link from 'components/Link'
 import TextField from 'components/TextField'
 import { FormWrapper, FormLink } from 'components/Form'
+import React, { useState } from 'react'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import { useMutation } from '@apollo/client'
+import { MUTATION_REGISTER } from 'graphql/mutations'
+import { MutationRegister, MutationRegisterVariables } from 'graphql/generated/MutationRegister'
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form>
-      <TextField startIcon={<Icon label="AccountCircle" />} placeholder="Name" id="name" />
-      <TextField startIcon={<Icon label="Email" />} placeholder="Email" id="email" />
-      <TextField
-        startIcon={<Icon label="Lock" />}
-        placeholder="Password"
-        type="password"
-        name="password"
-      />
-      <TextField
-        startIcon={<Icon label="Lock" />}
-        placeholder="Confirm password"
-        type="password"
-        name="confirm-password"
-      />
+interface ValuesProps extends UsersPermissionsRegisterInput {
+  confirmPassword: string
+}
 
-      <Button fullWidth as="a" href="#" size="large">
-        Sign up now
-      </Button>
-      <FormLink>
-        Already have an account? <Link href="/login">Sign In</Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+const FormSignUp = () => {
+  const [values, setValues] = useState<ValuesProps>({
+    email: '',
+    password: '',
+    username: '',
+    confirmPassword: ''
+  })
+
+  const [createUser] = useMutation<MutationRegister, MutationRegisterVariables>(MUTATION_REGISTER)
+  const handleInput = (field: keyof typeof values) => (value: string) => {
+    setValues(s => ({
+      ...s,
+      [field]: value
+    }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent): Promise<void> => {
+    event.preventDefault()
+
+    await createUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          startIcon={<Icon label="AccountCircle" />}
+          placeholder="Username"
+          name="username"
+          onInputChange={handleInput('username')}
+        />
+        <TextField
+          startIcon={<Icon label="Email" />}
+          placeholder="Email"
+          name="email"
+          type="email"
+          onInputChange={handleInput('email')}
+        />
+        <TextField
+          startIcon={<Icon label="Lock" />}
+          placeholder="Password"
+          type="password"
+          name="password"
+          onInputChange={handleInput('password')}
+        />
+        <TextField
+          startIcon={<Icon label="Lock" />}
+          placeholder="Confirm password"
+          type="password"
+          name="confirm-password"
+          onInputChange={handleInput('confirmPassword')}
+        />
+
+        <Button fullWidth size="large" type="submit">
+          Sign up now
+        </Button>
+        <FormLink>
+          Already have an account? <Link href="/login">Sign In</Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
