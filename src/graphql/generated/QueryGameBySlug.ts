7066619d7dac11804ccfc2db875ae9d3b57c09f7
albertoflorence <alberto@alberto.localdomain>
2022-04-08 15:45:58 -0300

    add register mutation

--- src/graphql/generated/QueryGameBySlug.ts
+++ src/graphql/generated/QueryGameBySlug.ts
@@ -44,7 +44,6 @@ export interface QueryGameBySlug_games_platforms {
 export interface QueryGameBySlug_games {
   __typename: "Game";
   name: string;
-  slug: string;
   short_description: string;
   description: string;
   price: number;
