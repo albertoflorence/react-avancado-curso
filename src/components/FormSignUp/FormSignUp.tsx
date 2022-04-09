3abd3c90452ccbe5de5581faf6b20c9c2bf1f4f9
albertoflorence <alberto@alberto.localdomain>
2022-04-08 22:46:06 -0300

    update signup to login after signing up

--- src/components/FormSignUp/FormSignUp.tsx
+++ src/components/FormSignUp/FormSignUp.tsx
@@ -4,24 +4,33 @@ import Link from 'components/Link'
 import TextField from 'components/TextField'
 import { FormWrapper, FormLink } from 'components/Form'
 import React, { useState } from 'react'
-import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import { useMutation } from '@apollo/client'
 import { MUTATION_REGISTER } from 'graphql/mutations'
 import { MutationRegister, MutationRegisterVariables } from 'graphql/generated/MutationRegister'
-
-interface ValuesProps extends UsersPermissionsRegisterInput {
-  confirmPassword: string
-}
+import { signIn } from 'next-auth/react'
 
 const FormSignUp = () => {
-  const [values, setValues] = useState<ValuesProps>({
+  const [values, setValues] = useState({
     email: '',
     password: '',
     username: '',
     confirmPassword: ''
   })
 
-  const [createUser] = useMutation<MutationRegister, MutationRegisterVariables>(MUTATION_REGISTER)
+  const [createUser, { error, loading }] = useMutation<MutationRegister, MutationRegisterVariables>(
+    MUTATION_REGISTER,
+    {
+      onError: error => console.log(error),
+      onCompleted: () =>
+        !error &&
+        signIn<'credentials'>('credentials', {
+          email: values.email,
+          password: values.password,
+          callbackUrl: '/'
+        })
+    }
+  )
+
   const handleInput = (field: keyof typeof values) => (value: string) => {
     setValues(s => ({
       ...s,
@@ -74,7 +83,7 @@ const FormSignUp = () => {
           onInputChange={handleInput('confirmPassword')}
         />
 
-        <Button fullWidth size="large" type="submit">
+        <Button fullWidth size="large" type="submit" loading={loading}>
           Sign up now
         </Button>
         <FormLink>
