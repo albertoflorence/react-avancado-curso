c4b01451f6a309277129cf66893e95890c277e33
albertoflorence <alberto@alberto.localdomain>
2022-04-09 00:10:56 -0300

    add logout page

---
+++ src/pages/logout.tsx
@@ -0,0 +1,11 @@
+import { signOut } from 'next-auth/react'
+import { useEffect } from 'react'
+
+export default function Logout() {
+  useEffect(() => {
+    signOut({
+      callbackUrl: '/'
+    })
+  })
+  return <></>
+}
