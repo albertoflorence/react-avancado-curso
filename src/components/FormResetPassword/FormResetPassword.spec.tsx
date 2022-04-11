1ec4a704b0c887f33c7ca6ebaf266ecfdebbed73
albertoflorence <alberto@alberto.localdomain>
2022-04-11 18:35:02 -0300

    add FormResetPassword tests

--- src/components/FormResetPassword/FormResetPassword.spec.tsx
+++ src/components/FormResetPassword/FormResetPassword.spec.tsx
@@ -1,6 +1,9 @@
 import { MockedProvider } from '@apollo/client/testing'
-import { render, screen } from 'utils/tests'
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from 'utils/tests'
+import { useRouter } from '../../../.jest/useRouter.mock'
 import FormForgotPassword from './FormResetPassword'
+import 'server.mock'
 
 const init = () => {
   return render(
@@ -10,12 +13,49 @@ const init = () => {
   )
 }
 
+const getPasswordInput = () => screen.getByPlaceholderText('Password')
+const getConfirmPasswordInput = () => screen.getByPlaceholderText('Confirm password')
+const getButton = () => screen.getByRole('button', { name: /send email/i })
+
 describe('<FormForgotPassword />', () => {
   it('should render correctly', () => {
     init()
 
-    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
-    expect(screen.getByPlaceholderText('Confirm password')).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: /send email/i })).toBeInTheDocument()
+    expect(getPasswordInput()).toBeInTheDocument()
+    expect(getConfirmPasswordInput()).toBeInTheDocument()
+    expect(getButton()).toBeInTheDocument()
+  })
+
+  it('should show validation error', async () => {
+    init()
+    userEvent.type(getPasswordInput(), 'Senha12345')
+    userEvent.type(getConfirmPasswordInput(), 'Senha54321')
+    userEvent.click(getButton())
+
+    expect(await screen.findByText(/confirm password must match the password/i)).toBeInTheDocument()
+  })
+
+  it('should show invalid code error', async () => {
+    useRouter.mockImplementation(() => ({ query: { code: 'invalid code' } } as never))
+    init()
+    const password = 'Senha12345'
+    userEvent.type(getPasswordInput(), password)
+    userEvent.type(getConfirmPasswordInput(), password)
+    userEvent.click(getButton())
+
+    expect(await screen.findByText(/invalid params/i)).toBeInTheDocument()
+  })
+
+  it('should redirect to login after password reset', async () => {
+    const push = jest.fn()
+    useRouter.mockImplementation(() => ({ query: { code: 'valid code' }, push } as never))
+    init()
+    const password = 'Senha12345'
+    userEvent.type(getPasswordInput(), password)
+    userEvent.type(getConfirmPasswordInput(), password)
+    userEvent.click(getButton())
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith('/login')
+    })
   })
 })
--- src/utils/tests/mockServer/handlers.ts
+++ src/utils/tests/mockServer/handlers.ts
@@ -1,30 +1,56 @@
 import { rest } from 'msw'
+import { ResetPasswordProps } from 'services/register'
+
+const getUrl = (url: string): string => process.env.NEXT_PUBLIC_API_URL + url
 
 interface LoginReqBody {
   email: string
 }
 
-export const handlers = [
-  rest.post<LoginReqBody>(
-    process.env.NEXT_PUBLIC_API_URL + '/auth/forgot-password',
-    (req, res, ctx) => {
-      const { email } = req.body
+const forgotPassword = rest.post<LoginReqBody>(
+  process.env.NEXT_PUBLIC_API_URL + '/auth/forgot-password',
+  (req, res, ctx) => {
+    const { email } = req.body
 
-      if (email.includes('invalid')) {
-        return res(
-          ctx.status(400),
-          ctx.json({
-            message: [{ messages: [{ message: 'This email does not exist' }] }]
-          })
-        )
-      } else {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            ok: true
-          })
-        )
-      }
+    if (email.includes('invalid')) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: [{ messages: [{ message: 'This email does not exist' }] }]
+        })
+      )
+    } else {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
     }
-  )
-]
+  }
+)
+
+const resetPassword = rest.post<ResetPasswordProps>(
+  getUrl('/auth/reset-password'),
+  (req, res, ctx) => {
+    const { code, password, passwordConfirmation } = req.body
+
+    if (code.includes('invalid') || password !== passwordConfirmation) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: [{ messages: [{ message: 'invalid params' }] }]
+        })
+      )
+    } else {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
+    }
+  }
+)
+
+export const handlers = [forgotPassword, resetPassword]
