2cd160232e9d6f9667da3b0c508fb56013f0575a
albertoflorence <alberto@alberto.localdomain>
2022-04-14 20:23:01 -0300

    change cart page to redirect user if not logged in

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,15 +1,21 @@
 import Cart, { CartTemplateProps } from 'templates/Cart'
 import mockPayments from 'components/PaymentOptions/mock'
 import { getRecommended } from 'services'
+import { GetServerSidePropsContext } from 'next/types'
+import protectedRoutes from 'utils/protectedRoutes'
 
 export default function Index(props: CartTemplateProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps(): Promise<{ props: CartTemplateProps }> {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  if (!session) return { props: {} }
+
   const recommended = await getRecommended()
   return {
     props: {
+      session,
       recommended,
       paymentOptions: {
         cards: mockPayments
--- src/utils/protectedRoutes.ts
+++ src/utils/protectedRoutes.ts
@@ -4,9 +4,8 @@ import { getSession } from 'next-auth/react'
 async function protectedRoutes(context: GetServerSidePropsContext) {
   const session = await getSession(context)
   if (!session) {
-    context.res.writeHead(302, {
-      Location: '/login?callbackUrl=' + context.resolvedUrl
-    })
+    context.res.setHeader('Location', '/login?callbackUrl=' + context.resolvedUrl)
+    context.res.statusCode = 302
     context.res.end()
   }
 
