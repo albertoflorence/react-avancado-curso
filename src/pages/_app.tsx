36bb5917cefb8ad90c625379d52f3d36338578e8
albertoflorence <alberto@alberto.localdomain>
2022-04-19 05:09:05 -0300

    change manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,7 @@ function App({ Component, pageProps }: AppProps) {
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
-                <meta name="description" content="A simple project" />
+                <meta name="description" content="Online Game Store" />
               </Head>
               <DefaultSeo {...SEO} />
               <GlobalStyles />
