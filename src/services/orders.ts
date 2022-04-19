8555611a1784cec11541f8ab30c7e32168580a66
albertoflorence <alberto@alberto.localdomain>
2022-04-19 04:04:38 -0300

    change fetch policy of getOrders

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -9,7 +9,8 @@ export const getOrders = async (id: number, session: Session) => {
   const client = initializeApollo(undefined, session)
   const { data } = await client.query<QueryOrders, QueryOrdersVariables>({
     query: QUERY_ORDERS,
-    variables: { id }
+    variables: { id },
+    fetchPolicy: 'no-cache'
   })
   const mappedOrder = data.orders.reduce(
     (arr, order) => arr.concat(...order.games.map(() => mapperOrder(order))),
